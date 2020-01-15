38926dbfda3caa93e51da7dcb97a6994c36ccbe1
mateucamps <mcampshoms@gmail.com>
2020-01-15 16:24:06 +0100

    anglès

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import argparse
 from kramer import paquet
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', type=str, help="Port sèrie del dispositiu (ex. COM3)", required=True)
-parser.add_argument('-i', type=int,  help="Canal entrada", required=True)
-parser.add_argument('-o', type=int,  help="Canal sortida", required=True)
+parser.add_argument('-p', type=str, help="Device serial port (ex. COM3)", required=True)
+parser.add_argument('-i', type=int,  help="Input channel", required=True)
+parser.add_argument('-o', type=int,  help="Output channel", required=True)
 
 args = parser.parse_args()
 
@@ -17,8 +17,8 @@ print('Baud: ' + 9600)
 cmd = paquet(args.i, args.o)
 nbytes = ser.write(cmd)
 
-print('Entrada: ' + str(args.i))
-print('Sortida: ' + str(args.o))
+print('Input: ' + str(args.i))
+print('Output: ' + str(args.o))
 
-print('Bytes escrits: ' + str(nbytes))
+print('Bytes written: ' + str(nbytes))
 ser.close()
